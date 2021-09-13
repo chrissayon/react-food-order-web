594146cecc94214986d47cd2f8891d0b0b6af346
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-09-13 23:01:35 +1000

    master: fix css reference

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -6,7 +6,7 @@ const Backdrop = props => {
 }
 
 const ModalOverlay = props => {
-    return <div className={classes.Modal}>
+    return <div className={classes.modal}>
         <div className={classes.content}>{props.children}</div>
     </div>
 }
